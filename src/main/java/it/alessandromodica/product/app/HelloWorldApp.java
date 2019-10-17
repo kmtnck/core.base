74c3ff02d723064ef31b0892e8c9a37eb313b09b
kmtnck <alessandro.modica@gmail.com>
2019-10-17 15:05:14 +0200

    javadoc sulla classe helloworldapp

--- src/main/java/it/alessandromodica/product/app/HelloWorldApp.java
+++ src/main/java/it/alessandromodica/product/app/HelloWorldApp.java
@@ -8,28 +8,18 @@ import it.alessandromodica.product.common.exceptions.BusinessException;
 public class HelloWorldApp extends MainApplication {
 
 	/**
-	 * E' un metodo a titolo di esempio su cui la convivenza tra esigenze legacy
-	 * e attuali è sempre fattibile
+	 * Metodo di inizializzazione del contesto applicativo. Inizializza il log4j e
+	 * crea il manager session factory hibernate per accerere allo storage.
 	 * 
-	 * Si vuole adattare un set di viste particolarmente complesse e delegare a
-	 * hibernate la sola mappatura del resultset Con hdbsql configurato in
-	 * modalità default, ad ogni creazione dello schema vengono create le viste
-	 * mappate dalle entità opportune
-	 * 
-	 * Un test esemplifica questo flusso di innesto chiamato
-	 * testHelloWorldUtentiConnessi
-	 * 
-	 * La MainApplication è il punto di partenza per implementare moduli e
-	 * componenti personalizzati. Un esempio è il modulo consultazione, ma è
-	 * solo un esempio.
+	 * Questo metodo potrebbe inizializzare tutto cio che potrebbe servire ad una
+	 * applicazione piu complessa.
 	 * 
 	 * @throws BusinessException
 	 */
-
 	public static void InitApp() throws BusinessException {
 
 		TITOLO_APP = "Una nuova app che fa cose";
-		
+
 		InitApp("appjpa-mysql");
 
 	}
