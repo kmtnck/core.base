cf75cfc5204fa64ab135f7e10f10935a36ea856b
amodica <amodica@pc-amodica.ditechsw.it>
2020-05-27 16:17:44 +0200

    aggiunto il datasource.properties il quale è annotato su appconfig per parametrizzare le impostazioni di accesso al datasource

--- src/main/java/it/alessandromodica/product/common/config/AppConfig.java
+++ src/main/java/it/alessandromodica/product/common/config/AppConfig.java
@@ -4,10 +4,12 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -20,17 +22,30 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan(basePackages = "it.alessandromodica.product")
-//@EnableWebMvc
 @EnableTransactionManagement
+@PropertySource("classpath:datasource.properties")
 public class AppConfig {
+
+	@Value("${driver}")
+	private String driver;
+	@Value("${urids}")
+	private String urids;
+	@Value("${userdb}")
+	private String userdb;
+	@Value("${pwd}")
+	private String pwd;
+	@Value("${packagetoscan}")
+	private String packagetoscan;
+	@Value("${dialect}")
+	private String dialect;
 
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://svr1.alessandromodica.com/aless175_gotobusiness?useSSL=true");
-		dataSource.setUsername("aless175_kmtnck");
-		dataSource.setPassword("Nsl80cdm");
+		dataSource.setDriverClassName(driver);
+		dataSource.setUrl(urids);
+		dataSource.setUsername(userdb);
+		dataSource.setPassword(pwd);
 		return dataSource;
 	}
 
@@ -38,7 +53,7 @@ public class AppConfig {
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		em.setDataSource(dataSource());
-		em.setPackagesToScan(new String[] { "it.alessandromodica.product.model.po" });
+		em.setPackagesToScan(new String[] { packagetoscan });
 
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		em.setJpaVendorAdapter(vendorAdapter);
@@ -46,27 +61,27 @@ public class AppConfig {
 
 		return em;
 	}
-	
+
 	@Bean
 	public PlatformTransactionManager transactionManager() {
-	    JpaTransactionManager transactionManager = new JpaTransactionManager();
-	    transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-	 
-	    return transactionManager;
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+		return transactionManager;
 	}
-	 
+
 	@Bean
-	public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
-	    return new PersistenceExceptionTranslationPostProcessor();
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
 	}
-	 
+
 	Properties additionalProperties() {
-	    Properties properties = new Properties();
-	    properties.setProperty("hibernate.hbm2ddl.auto", "update");
-	    properties.setProperty("hibernate.dialect", "org.hibernate.spatial.dialect.mysql.MySQL56SpatialDialect");
-	    properties.setProperty("hibernate.show_sql", "true");
-	    
-	    return properties;
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", dialect);
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.show_sql", "true");
+
+		return properties;
 	}
 
 }
