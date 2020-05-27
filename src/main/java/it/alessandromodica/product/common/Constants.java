a697797d70c9cd99d7f8c1c91ecbdd80b776f5f3
amodica <amodica@pc-amodica.ditechsw.it>
2020-05-27 17:20:13 +0200

    predisposizione alla configurazione di swagger

---
+++ src/main/java/it/alessandromodica/product/common/Constants.java
@@ -0,0 +1,15 @@
+package it.alessandromodica.product.common;
+
+/**
+ * Costanti comuni
+ * 
+ * @author mgambini
+ *
+ */
+public class Constants {
+	
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String API_DEFAULT_PATH_PREFIX = "/api";
+
+}
--- src/main/java/it/alessandromodica/product/rest/IEntityRest.java
+++ src/main/java/it/alessandromodica/product/rest/IEntityRest.java
@@ -1,11 +1,12 @@
 package it.alessandromodica.product.rest;
 
-
 import java.util.List;
 
 import javax.ws.rs.core.UriInfo;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import it.alessandromodica.product.common.exceptions.RepositoryException;
 import it.alessandromodica.product.persistence.searcher.BOSerializeCriteria;
 
@@ -20,10 +21,16 @@ import it.alessandromodica.product.persistence.searcher.BOSerializeCriteria;
 @Api(value = "modulo.entity", description = "Interfaccia per la gestione CRUD di una entita database")
 public interface IEntityRest<T> {
 
+	@ApiOperation(value = "Test chiamata rest", response = Object.class)
+	@ApiParam(value = "Id per il test")
 	public Object test(String id);
-	
+
+	@ApiOperation(value = "Recupera un oggetto da database", response = Object.class)
+	@ApiParam(value = "ID dell'oggetto")
 	public Object get(int id, UriInfo info) throws RepositoryException;
 
+	@ApiOperation(value = "Recupera un oggetto da database", response = List.class)
+	@ApiParam(value = "Token di ricerca")
 	public List<Object> ricerca(BOSerializeCriteria searcher, UriInfo info) throws RepositoryException;
 
 	public int count(BOSerializeCriteria searcher, UriInfo info) throws RepositoryException;
